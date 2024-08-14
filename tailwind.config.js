8c49d6247c9e39a2fb76e558e3f823ad6b2bb541
Alistair Cass <alistair.cass@riskhub.co.uk>
2024-08-14 10:38:43 +0100

    Fix + Light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,vue,ts}',
+  ],
+  darkMode: 'selector',
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
